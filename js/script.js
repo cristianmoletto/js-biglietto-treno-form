fa0b99e1117d7f4b4efac92170ce18992da0d08c
cristianmoletto <cristian.moletto@gmail.com>
2025-10-01 19:48:23 +0200

    feat: js prende i dati dal form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("prova")
+const formElem = document.getElementById("form")
+const kmElem = document.getElementById("km");
+const ageElem = document.getElementById("age");
+// const btnElem = document.getElementById("btn");
+
+form.addEventListener("submit",function(event){
+event.preventDefault();
+console.log(kmElem.value);
+console.log(ageElem.value);
+}
+)
