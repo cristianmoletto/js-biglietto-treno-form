699cd327b93543153184d801d5f719f882d4c279
cristianmoletto <cristian.moletto@gmail.com>
2025-10-02 18:26:20 +0200

    feat: aggiunto boostrap e risultato finale su dom

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
-const formElem = document.getElementById("form")
+const formElem = document.getElementById("form");
 const kmElem = document.getElementById("km");
 const ageElem = document.getElementById("age");
+let risultato = document.getElementById("risultato");
 // const btnElem = document.getElementById("btn");
 
-form.addEventListener("submit",function(event){
-event.preventDefault();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  const age = parseInt(ageElem.value);
+  const km = parseInt(kmElem.value);
+  console.log("km",km)
 
-const age = parseInt(kmElem.value);
-const km = parseFloat(ageElem.value);
-
-let message = "";
   let finalPrice = 0;
+  let discount = 0;
   let basicPrice = km * 0.21;
+  console.log(basicPrice)
 
   if (age > 65) {
-    finalPrice = basicPrice - basicPrice * 0.4;
+    discount = 0.4;
+    finalPrice = basicPrice - basicPrice * discount;
   } else if (age < 18) {
-    finalPrice = basicPrice - basicPrice * 0.2;
+    discount = 0.2;
+    finalPrice = basicPrice - basicPrice * discount;
   } else {
     finalPrice = basicPrice;
   }
 
   const finalPriceFormatted = finalPrice.toFixed(2);
+  formElem.reset();
 
-  message = `Il prezzo del biglietto è di € ${finalPriceFormatted}`;
+  risultato.innerHTML = 
+  `Sconto applicato del ${discount*100}%.\n\nIl prezzo del biglietto è di € ${finalPriceFormatted}`;
 
-  console.log(message);
+});
 
-}
-)
