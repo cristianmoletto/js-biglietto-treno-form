76521261c9a145711bfbbf5ee084387cdfb3bc66
cristianmoletto <cristian.moletto@gmail.com>
2025-10-02 12:23:09 +0200

    feat: milestone 1 raggiunto

--- js/script.js
+++ js/script.js
@@ -5,7 +5,28 @@ const ageElem = document.getElementById("age");
 
 form.addEventListener("submit",function(event){
 event.preventDefault();
-console.log(kmElem.value);
-console.log(ageElem.value);
+
+
+const age = parseInt(kmElem.value);
+const km = parseFloat(ageElem.value);
+
+let message = "";
+  let finalPrice = 0;
+  let basicPrice = km * 0.21;
+
+  if (age > 65) {
+    finalPrice = basicPrice - basicPrice * 0.4;
+  } else if (age < 18) {
+    finalPrice = basicPrice - basicPrice * 0.2;
+  } else {
+    finalPrice = basicPrice;
+  }
+
+  const finalPriceFormatted = finalPrice.toFixed(2);
+
+  message = `Il prezzo del biglietto è di € ${finalPriceFormatted}`;
+
+  console.log(message);
+
 }
 )
